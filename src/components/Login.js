ca37629a50e3e192ca42b55f9f99bfe8878816d9
Shubham Awasthi <aw.shubh@gmail.com>
2019-03-18 21:37:29 +0530

    Updated Login UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,12 +3,12 @@ import io from 'socket.io-client';
 import { withRouter } from 'react-router-dom';
 import swal from 'sweetalert';
 import './../css/login.css';
-import c2cLogo from './../images/logowhite.png';
+import c2cLogo from './../images/C2C_Logo.svg';
 
 let userSocket;
 
 class Login extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             username: ''
@@ -26,7 +26,7 @@ class Login extends React.Component {
 
         });
 
-        userSocket.on('disconnect' , () => {
+        userSocket.on('disconnect', () => {
             console.log("disconnected");
         });
         this.usernameChange = this.usernameChange.bind(this);
@@ -34,32 +34,36 @@ class Login extends React.Component {
     }
 
     usernameChange = (e) => {
-        this.setState({username : e.target.value});
+        this.setState({ username: e.target.value });
     }
     handleJoin = () => {
-        console.log("username is : "+this.state.username);
+        console.log("username is : " + this.state.username);
         let username = this.state.username;
         localStorage.setItem('username', username);
-        userSocket.emit('join', {username: username}, (err) => {
+        userSocket.emit('join', { username: username }, (err) => {
             console.log(err);
-            swal("Oops!", err.err , "error");
-            this.setState({username : ''});
+            swal("Oops!", err.err, "error");
+            this.setState({ username: '' });
         });
     }
-    render(){
-        return(
-            <div className="login-container">
-                <div className="c2cLogo" >
-                    <img src={ c2cLogo }  alt="code2create" />
+    render() {
+        return (
+            <div className="login-container" style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+                <div style={{ flex: 2, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <img src={c2cLogo} alt="code2create" height="150px" />
                 </div>
-                <div className="login-wrapper z-depth-5">
-                    <h4>Username: </h4>
-                    <input type="text" onChange={this.usernameChange} />
+                <div style={{ flex: 2, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <div className="login-wrapper z-depth-5">
+                        <h4 style={{margin:0}}>Username: </h4>
+                        <input style={{color:'white'}} type="text" onChange={this.usernameChange} />
 
-                    <input type="submit" value="Join" className="btn" onClick={this.handleJoin} />
+                        <input type="submit" value="Join" className="btn" onClick={this.handleJoin} />
+                    </div>
                 </div>
-                <div className="footer" style={{ color:'#4a4a4a' }}> 
-                    <p>Developed With <i className="fa fa-heart"></i> by ACM</p>
+                <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <div className="footer" style={{ color: '#4a4a4a' }} style={{ display: 'flex', flex: 1 }}>
+                        <p>Developed With <i className="fa fa-heart"></i> by ACM</p>
+                    </div>
                 </div>
             </div>
         );
